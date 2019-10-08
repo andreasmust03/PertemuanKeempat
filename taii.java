7c6aca916b3d2a85dc4ece3e300682e700edfa54
Toro7 <55041478+Toro7@users.noreply.github.com>
2019-10-08 13:32:13 +0700

    Create taii.java

---
+++ taii.java
@@ -0,0 +1,42 @@
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Toshiba
+ */
+public class ConttohArrayDuaDimensiImmutable {
+    public static void main(String[] args) {
+        
+        String[][] biodatas = new String[6][2];
+        
+        biodatas[0][0] = "riki";
+        biodatas[0][1] = "widi";
+        
+        biodatas[1][0] = "anto";
+        biodatas[1][1] = "toro";
+        
+        biodatas[2][0] = "ges";
+        biodatas[2][1] = "sang";
+        
+        biodatas[3][0] = "pra";
+        biodatas[3][1] = "yoga";
+        
+        biodatas[4][0] = "muhammad";
+        biodatas[4][1] = "fa";
+        
+        biodatas[5][0] = "i";
+        biodatas[5][1] = "cal";
+        
+        System.out.println("Nama saya adalah: " + biodatas[5][0] + " dan kembaran saya adalah: " + biodatas[5][1]);
+    
+        
+        
+    }
+}
