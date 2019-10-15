25b19b77d2b9d23ee9185bf84e2c743ba889f6ec
warokdroid2 <56612320+warokdroid2@users.noreply.github.com>
2019-10-16 05:33:49 +0700

    Buat ContohArrayDuaDimensiMutable.java

---
+++ ContohArrayDuaDimensiMutable.java
@@ -0,0 +1,17 @@
+public class ContohArrayDuaDimensiMutable {
+
+    public static void main(String[] args) {
+
+        String[][] biodatas = {
+                {"Fitriyanti", "Sulawesi"},
+                {"Innas", "Ponorogo"},
+                {"Faisal", "Madiun"},
+                {"Riki", "Ponorogo"},
+                {"Gesang", "Madiun"},
+                {"Erik", "Ponorogo"}
+        };
+
+        System.out.println(biodatas[0][0]);
+        System.out.println(biodatas[4][1]);
+    }
+}
